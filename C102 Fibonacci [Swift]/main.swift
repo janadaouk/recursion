fbc64809e8da3d1b201c55fe91a7590857986438
jana-daouk <jana.daouk@student.allenisd.org>
2022-11-17 14:43:41 -0600

    First Commit

---
+++ C102 Fibonacci [Swift]/main.swift
@@ -0,0 +1,26 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+func fib(int: Int) -> Int {
+    if(int <= 2){
+        print("Calculating fibonacci(\(int))...") 
+        print("Terminal case, returning 1.")
+        return 1
+    }
+    else{
+        print("Calculating fibonacci(\(int))...")
+        let result = fib(int: int-1) + fib(int: int-2)
+        print("Non-terminal case, returning \(result).")
+        return result
+    }
+}
+let finalResult = fib(int: n)
+print("The final result is: \(finalResult).")
