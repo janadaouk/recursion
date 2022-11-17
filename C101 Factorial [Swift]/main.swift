46e90ae647e86175dbeab850e1823a9813b909a5
jana-daouk <jana.daouk@student.allenisd.org>
2022-11-17 14:57:56 -0600

    with best coding

--- C101 Factorial [Swift]/main.swift
+++ C101 Factorial [Swift]/main.swift
@@ -8,19 +8,23 @@ let n = 7
 
 
 // Add your code below:
-
+// it calculates the factorial of the given input and gives details of the process
 func factorial(n: Int) -> Int {
     if(n <= 1){
+ // the answer here will always be one if it passes the 1 condition
         print("Calculating factorial(\(n))...")   
         print("Terminal case, returning 1.")
         return 1
     }
     else {
+         // current number it is working on     
         print("Calculating factorial(\(n))...")
+         // moves on to another number   
         let result =  n*factorial(n: n-1)
         print("Non-terminal case, returning \(result).")
         return result        
     }    
 }
+// prints the final result     
 let finalResult = factorial(n: n)
 print("The final result is: \(finalResult).")                   
--- C102 Fibonacci [Swift]/main.swift
+++ C102 Fibonacci [Swift]/main.swift
@@ -8,19 +8,23 @@ let n = 7
 
 
 // Add your code below:
-
+// function that calculates the next fibonacii number based on the given input and prints the process in detail   
 func fib(int: Int) -> Int {
     if(int <= 2){
+        // the answer here will always be one if it passes the 1 condition
         print("Calculating fibonacci(\(int))...") 
         print("Terminal case, returning 1.")
         return 1
     }
     else{
+         // current number it is working on  
         print("Calculating fibonacci(\(int))...")
+         // moves on to another number 
         let result = fib(int: int-1) + fib(int: int-2)
         print("Non-terminal case, returning \(result).")
         return result
     }
 }
+// prints the final result     
 let finalResult = fib(int: n)
 print("The final result is: \(finalResult).")
